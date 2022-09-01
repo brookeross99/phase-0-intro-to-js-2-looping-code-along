8f22f5523faff57533db521ee0e01bff3be48f04
Brooke Ross <brooke.ross2800@gmail.com>
2022-09-01 11:02:13 -0600

    commit tree 1e867210c1860899a6a8f8973e34295ddd4c2dc1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+//this is wrong--
+//function writeCards= (name) => `Thank you,${name}, for the wonderful surprise gift! `
+
+//const name = ["Guadalupe","Ollie","Aki",]
+//failed test:
+// function writeCards(name,event){
+//     let i=0;
+//     while(i<name.length){
+//         console.log(`Thank you,${name[i]},for the wonderful ${event} gift!`);
+//         i++;
+//     }
+//     return name
+// }
+// writeCards(name);
+
+function writeCards(name,event){
+    const thankYouCards = []
+    for(let i=0; i<name.length; i++){
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+       // debugger;
+    }
+    return thankYouCards
+}
+//writeCards(name);
+
+function countDown(number){
+   while(number>0){
+        console.log(number);
+        number-=1;
+        debugger;
+    }
+    console.log(number);
+}
